ba62d6703da002c558e1b865a6f29bcff54c3051
HelenArkh <alena.arkh@gmail.com>
2021-01-03 12:32:58 +0300

    Создала проект, изучила ссылочные и значимые типы данных

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            // 1
+            Method();
+            // 3
+        }
+
+        static void Method()
+        {
+            var x = 1;
+            int y = 2;
+            // 2
+        }
+    }
+}
